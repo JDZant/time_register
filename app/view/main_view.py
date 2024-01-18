14b260f658cd51bf899ae87ea789535c74326cfc
JDZant <jos.d.zant@gmail.com>
2024-01-18 21:35:05 +0100

    Set the main class as target script path Added models

--- app/view/main_view.py
+++ app/view/main_view.py
@@ -1,12 +1,12 @@
 # main_view.py
-import sys
 from datetime import datetime
 import tkinter as tk
 from tkcalendar import DateEntry
-
-sys.path.append('//')  # Add this line before your import
-
-from app.main import run_time_registration
+from app.controller.authentication_controller import Login
+from app.controller.time_registration_controller import TimeRegistration
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 class MainWindow(tk.Frame):
@@ -78,11 +78,16 @@ class MainWindow(tk.Frame):
             'time_registration_duration': self.time_registration_duration.get(),
         }
 
-        run_time_registration(user_config)
+        # Instantiate the controllers
+        service = Service('/usr/bin/chromedriver')
+        driver = webdriver.Chrome(service=service)
+        username = os.getenv('TIME_REG_USER')
+        password = os.getenv('TIME_REG_PASS')
 
+        login = Login(driver, user_config['date'])
+        login.login(username, password)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = MainWindow(root)
-    app.grid()
-    root.mainloop()
+        TimeRegistration(driver, user_config)
+
+        # Close the browser once done
+        driver.quit()
